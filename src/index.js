eb3d8cbddbd5ef7f7f46d67cff6863d76c087c75
TitanV <a01098930999@gmail.com>
2020-07-16 01:58:09 +0900

    update - react route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
 const koaBody = require('koa-body');
+const serve = require('koa-static');
 const db = require('./db');
 const {createReadStream} = require('fs');
 const cors = require('@koa/cors');
@@ -16,13 +17,15 @@ const models = db.models;
 
 const api = require('./api');
 
+const indexHtml = fs.readFileSync(path.resolve(__dirname, './public/index.html'), { encoding: 'utf8' });
 app.use(cors());
 app.use(logger());//http 메소드 로거 사용
 app.use(koaBody());//json 형식 파싱용
 
+app.use(serve(path.resolve(__dirname, './public')));
 router.get('/',(ctx) => {
   ctx.type = 'html';
-  ctx.render = ('./src/public/index.html');
+  ctx.render = (__dirname,'./public/index.html');
 });
 
 
