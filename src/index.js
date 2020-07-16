a82c23a96ae6ce830e7efdaf4545ca39a0110178
TitanV <a01098930999@gmail.com>
2020-07-16 09:43:49 +0900

    update - test

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const cors = require('@koa/cors');
 
 const fs = require('fs');
 const path = require('path');
-//const render = require('./server/render/index.js');
 
 const app = new Koa();
 const router = new Router();
@@ -22,10 +21,17 @@ app.use(cors());
 app.use(logger());//http 메소드 로거 사용
 app.use(koaBody());//json 형식 파싱용
 
+
+/*
 app.use(serve(path.resolve(__dirname, './public')));
+
 router.get('/',(ctx) => {
   ctx.body = indexHtml;
+   ctx.render = (__dirname,'./public/index.html');
 });
+
+*/
+
 
 
 router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
